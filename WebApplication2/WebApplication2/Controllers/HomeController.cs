577439e3ec78d54597f6af84678dbf866bf74c6f
SabirQ <sabirmg@code.edu.az>
2022-06-03 06:15:35 +0400

    Task Added

---
+++ WebApplication2/WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController:Controller
+    {
+        public IActionResult index()
+        {
+            return View();
+        }
+        public IActionResult Blogs()
+        {
+          
+            return View();
+            
+        }
+    }
+}
---
+++ WebApplication2/WebApplication2/Migrations/20220603021412_createdSecondTable.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApplication2.Migrations
+{
+    public partial class createdSecondTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Blogs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(nullable: true),
+                    Sub_title = table.Column<string>(nullable: true),
+                    Article = table.Column<string>(nullable: true),
+                    Image = table.Column<string>(nullable: true),
+                    Url = table.Column<string>(nullable: true),
+                    Publish_Date = table.Column<DateTime>(nullable: false),
+                    Comments_Quantity = table.Column<int>(nullable: false),
+                    Order = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Blogs", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Blogs");
+        }
+    }
+}
